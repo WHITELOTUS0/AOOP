9166c75d50654027a144ee804e90f91dbd1cfd48
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-05-09 18:03:39 +0300

    Update TestGenerics.java

--- TestGenerics.java
+++ TestGenerics.java
@@ -1,5 +1,10 @@
+//Generic Method to print arrays
+
 public class TestGenerics {
     public <E> void  printArray(E[] elements){
+        for(E element: elements){
+            System.out.println(element);
+        }
 
     }
 }
