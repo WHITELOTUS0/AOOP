2dd183dd174ed834314e045ea7793f1e52d676ab
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-05-09 18:22:08 +0300

    Create TestGeneric2.java

---
+++ TestGeneric2.java
@@ -0,0 +1,15 @@
+public class TestGeneric2 {
+    public <T> T m(T anyType){
+        return anyType;
+    }
+
+    public static void main(String[] args) {
+        TestGeneric2 test = new TestGeneric2();
+        String s = test.m("hello");
+        System.out.println(s);
+        int n = test.m(5);
+        System.out.println(n);
+        
+    }
+    
+}
