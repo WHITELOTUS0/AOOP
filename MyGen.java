49b56292594bccea05475cfe9261a70846b76290
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-05-09 17:44:45 +0300

    Update MyGen.java

--- MyGen.java
+++ MyGen.java
@@ -1,7 +1,17 @@
+//Generic Class
 public class MyGen<T> {
     T number;
     MyGen(T number){
         this.number=number;
+    }
+
+    public T getNumber(){
+        return number;
+    }
+
+    public static void main(String[] args) {
+        MyGen<Integer> obj= new MyGen<Integer>(3);
+
     }
     
 }
