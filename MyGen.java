4eb923ead91234846c8994c02c90f843df38dc06
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-05-09 17:49:46 +0300

    Update MyGen.java

--- MyGen.java
+++ MyGen.java
@@ -12,6 +12,8 @@ public class MyGen<T> {
     public static void main(String[] args) {
         MyGen<Integer> obj= new MyGen<Integer>(3);
         System.out.println(obj.getNumber());
+        MyGen<Float> obj2 = new MyGen<Float>(2.5);
+        System.out.println(obj2.getNumber());
 
     }
     
