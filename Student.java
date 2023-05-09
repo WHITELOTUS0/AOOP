50a5ed357518b8de30841a63190103a35b8ca5d5
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-05-09 17:34:52 +0300

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,5 +1,13 @@
 //Implementattion of HashMap IN JAVA
 import java.util.*;
 public class Student{
-    
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Glorry", 20);
+        map.put("Steven", 26);
+        map.put("Shariff", 22);
+
+        System.out.println(map.get("Glorry"));
+
+    }
 }
